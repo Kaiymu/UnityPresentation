4063dd67e79e6c1b0e5c4c1b90ed0f7f8fbab8c0
Thomas Fontaine <thomas.fontaine@ubisoft.com>
2020-01-14 12:17:09 +0100

    Initial commit with the full presentation.

---
+++ PresentationUnity/Assets/Scripts/ChangeAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+{
+    private Animator _myAnimator;
+    private void Start()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            _myAnimator.SetTrigger("GoLeftRight");
+        } else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _myAnimator.SetTrigger("GoTopBottom");
+        }
+    }
+}
